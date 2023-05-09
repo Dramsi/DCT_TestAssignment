cf48179e2b9e0fd5aa932d4fb4f2c5666e443f8e
Dramsi <dramsi.pau@gmail.com>
2023-05-10 01:15:12 +0300

    update Coin Details window

--- DCT_TestAssignment/CoinDetailsWindow.xaml.cs
+++ DCT_TestAssignment/CoinDetailsWindow.xaml.cs
@@ -11,6 +11,16 @@ namespace DCT_TestAssignment
         {
             InitializeComponent();
             nameTextBox.Text = coin.name;
+            rankTextBox.Text = coin.rank;
+            symbolTextBox.Text = coin.symbol;
+            supplyTextBox.Text = coin.supply;
+            maxSupplyTextBox.Text = coin.maxSupply;
+            marketCapUsdTextBox.Text = coin.marketCapUsd;
+            volumeUsd24HrTextBox.Text = coin.volumeUsd24Hr;
+            priceUsdTextBox.Text = coin.priceUsd;
+            changePercent24HrTextBox.Text = coin.changePercent24Hr;
+            vwap24HrTextBox.Text = coin.vwap24Hr;
+            explorerTextBox.Text = coin.explorer;
         }
     }
 }
