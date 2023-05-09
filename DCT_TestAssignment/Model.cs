973ae75367a7eeda56492f35bd1d40f65332e0a5
Dramsi <dramsi.pau@gmail.com>
2023-05-09 21:35:38 +0300

    add Model.cs

---
+++ DCT_TestAssignment/Model.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace DCT_TestAssignment
+{
+    public class Model
+    {
+        public List<Coin> data { get; set; }
+        public long timestamp { get; set; }
+    }
+
+    public class Coin
+    {
+        public string id { get; set; }
+        public string rank { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public string supply { get; set; }
+        public string maxSupply { get; set; }
+        public string marketCapUsd { get; set; }
+        public string volumeUsd24Hr { get; set; }
+        public string priceUsd { get; set; }
+        public string changePercent24Hr { get; set; }
+        public string vwap24Hr { get; set; }
+        public string explorer { get; set; }
+    }
+}
